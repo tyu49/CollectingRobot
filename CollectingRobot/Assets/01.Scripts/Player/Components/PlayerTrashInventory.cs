3cb4f0557101e431cee01cdc192f14046da10dac
tyu49 <k10y4j9@gmail.com>
2026-08-19 23:47:26 +0900

    asdf

---
+++ CollectingRobot/Assets/01.Scripts/Player/Components/PlayerTrashInventory.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace _01.Scripts.Player.Components
+{
+    public class PlayerTrashInventory : MonoBehaviour
+    {
+        [SerializeField] private int maxCapacity;
+        [field : SerializeField] public int CurrentCapacity { get; private set; }
+        private Player _player;
+    }
+}
